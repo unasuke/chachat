d8a07d72429f0667fafb2d318302b07b3686c852
Yusuke Nakamura <yusuke1994525@gmail.com>
2022-05-07 00:27:26 +0900

    Create sessions controller and make loginable

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_or_create_by(uid: auth_hash[:uid]) do |user|
+      user.nickname = auth_hash[:info][:nickname]
+      user.image_url = auth_hash[:info][:image]
+    end
+    log_in user
+    redirect_to root_path
+  end
+
+  private
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'timeline#index'
+  get '/auth/:provider/callback', to: 'sessions#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
