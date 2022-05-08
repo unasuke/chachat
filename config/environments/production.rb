e93a17de36f50644301aca5f58f2a49a40f2ffe5
Yusuke Nakamura <yusuke1994525@gmail.com>
2022-05-08 15:55:46 +0900

    Add allowed host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.hosts << "chachatapp.herokuapp.com"
+  config.hosts << "chachat.up.railway.app"
 end
